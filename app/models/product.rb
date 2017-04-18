701f252e36af44f2e1660d346b2908d7822e3af6
Luis Felipe <actuario.felipe@gmail.com>
2017-04-17 21:40:33 -0500

    categories en seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :category, inverse_of: :products
+
+  validates :price, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Product.destroy_all
+
+category1 = Category.create(name: 'Category 1')
+category2 = Category.create(name: 'Category 2')
+category3= Category.create(name: 'Category 3')
+
+Product.create name: 'Product 1', price: 100, category_id: category1.id
+Product.create name: 'Product 2', price: 150, category_id: category1
+category1.products << Product.create(name: 'Producto 3', price: 150)
+
+product = category2.products.build(name: 'Product 4', price: 200)
+puts product.inspect
+product.save
+puts product.inspect
+product.destroy
+
+puts '----------------------------'
+pro=Product.last
+pro.premium = true
+pro.save
+puts pro.inspect
+puts '----------------------------'
+
+Category.all.each do |c|
+  c.name = c.name.upcase
+  c.save
+end
+
+Category.all.each {|c| puts c.name}
